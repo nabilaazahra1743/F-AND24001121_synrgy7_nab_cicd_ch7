6187b6416693e82749808a3149efbad83ae06d6a
nabilaazahra1743 <nabilazahra1734@gmail.com>
2024-08-09 19:26:39 +0700

    mvvm ch6

--- app/src/main/java/com/example/listingmovie/HomepageActivity.kt
+++ app/src/main/java/com/example/listingmovie/HomepageActivity.kt
@@ -1,47 +1,47 @@
-package com.example.listingmovie
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.listingmovie.adapter.MovieAdapter
-import com.example.listingmovie.databinding.ActivityHomepageBinding
-import com.example.listingmovie.models.Movie
-import com.example.listingmovie.models.MovieResponse
-import com.example.listingmovie.services.MovieApiInterface
-import com.example.listingmovie.services.MovieApiService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
-class HomepageActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityHomepageBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val binding = ActivityHomepageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.rvMovieList.layoutManager = LinearLayoutManager(this)
-        binding.rvMovieList.setHasFixedSize(true)
-
-        getMovieData { movies: List<Movie> ->
-            binding.rvMovieList.adapter = MovieAdapter(movies)
-        }
-    }
-
-    private fun getMovieData(callback: (List<Movie>) -> Unit){
-        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
-        apiService.getMovieList().enqueue(object: Callback<MovieResponse>{
-            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
-                return callback(response.body()!!.movies)
-            }
-
-            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-
-            }
-
-        })
-
-    }
-}
+//package com.example.listingmovie
+//
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import com.example.listingmovie.adapter.MovieAdapter
+//import com.example.listingmovie.databinding.ActivityHomepageBinding
+//import com.example.listingmovie.models.Movie
+//import com.example.listingmovie.models.MovieResponse
+//import com.example.listingmovie.services.MovieApiInterface
+//import com.example.listingmovie.services.MovieApiService
+//import retrofit2.Call
+//import retrofit2.Callback
+//import retrofit2.Response
+//
+//class HomepageActivity : AppCompatActivity() {
+//
+//    private lateinit var binding: ActivityHomepageBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        val binding = ActivityHomepageBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        binding.rvMovieList.layoutManager = LinearLayoutManager(this)
+//        binding.rvMovieList.setHasFixedSize(true)
+//
+//        getMovieData { movies: List<Movie> ->
+//            binding.rvMovieList.adapter = MovieAdapter(movies)
+//        }
+//    }
+//
+//    private fun getMovieData(callback: (List<Movie>) -> Unit){
+//        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
+//        apiService.getMovieList().enqueue(object: Callback<MovieResponse>{
+//            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+//                return callback(response.body()!!.movies)
+//            }
+//
+//            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+//
+//            }
+//
+//        })
+//
+//    }
+//}
